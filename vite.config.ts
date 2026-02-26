ef5d1d75ea0aabf794edb6190389755cd2c0cb52
Pro <pronit@tardis.digital>
2026-02-26 21:38:35 +0000

    fix(vite): add svelte plugin to main vite config so terminal.html served correctly

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { resolve } from "node:path";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svelte()],
   build: {
     target: "esnext",
     outDir: "dist/client",
